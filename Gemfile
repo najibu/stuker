8ac1f26e2e1e9caaca33d37df182061c0d5ff3be
najibu <ns.najibu@gmail.com>
2016-10-11 22:07:41 +0100

    Created Connections and setup oauth for twitter

--- Gemfile
+++ Gemfile
@@ -58,3 +58,5 @@ gem 'simple_form'
 gem 'devise' 
 
 gem 'dotenv-rails', groups: [:development]
+
+gem 'omniauth-twitter'
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,5 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
   validates_presence_of :time_zone
+  has_many :connections, dependent: :destroy
 end
