739263d750c5b5a4419d522f21e4bc67a384e8db
najibu <ns.najibu@gmail.com>
2016-10-22 14:47:52 +0100

    Added strong exception handling to omniauth

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -3,3 +3,6 @@ Rails.application.config.middleware.use OmniAuth::Builder do
 	provider :facebook, ENV["FACEBOOK_ID"], ENV["FACEBOOK_SECRET"], scope: 'email, publish_actions'
 end
 
+OmniAuth.config.on_failure = Proc.new do |env|
+	ConnectionsController.action(:omniauth_failure).call(env)
+end
